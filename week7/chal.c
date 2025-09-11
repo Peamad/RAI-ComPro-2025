feec342babef47166e5aa5c1c08d5f135f29cce9
Peamad <68011286@kmitl.ac.th>
2025-09-11 18:59:53 +0000

    7

--- week7/chal.c
+++ week7/chal.c
@@ -1,44 +1,62 @@
-#include<stdio.h>
-int main(){
-    int i;
-    struct student{
-        char name[20];
-        int id;
-        float avg,m,e,s,h,avm,ave,avs,avh;
-    }s[10];
+#include <stdio.h>
+int main() {
 
-    for(i=1;i<=3;i++){
-        printf("enter name , ID and 4 grades(math english science history) for student %d\n",i);
-        scanf("%s %d %f %f %f %f",s[i].name,&s[i].id,&s[i].m,&s[i].e,&s[i].s,&s[i].h);
-        s[i].avg=(s[i].m+s[i].e+s[i].s+s[i].h)/4;
-        s[i].avm=(s[1].m+s[2].m+s[3].m)/3
-        s[i].ave=(s[1].e+s[2].e+s[3].e)/3
-        s[i].avs=(s[1].s+s[2].s+s[3].s)/3
-        s[i].avh=(s[1].h+s[2].h+s[3].h)/3
+    struct student {
+    char name[20];   
+    int id;
+    float m, e, s, h; 
+    float avg;        
+    }st[3];
+    char subjName[4][10] = {"Math", "English", "Science", "History"};
+    float sumM=0,sumE=0,sumS=0,sumH=0;
+    float subjAvg[4];
 
-    }
-    printf("student averages :\n");
-    int max=0
-    for(i=1;i<=3;i++){
-        if(s[i].avg>s[max].avg)
-            max = i;
-        printf("%s(ID: %d): %.2f\n",s[i].name,s[i].id,s[i].avg);
-
-    }
-    int maxa=s[i].avm;
     
-    for(i=1;i<=3;i++){
-        if(s.[i].ave>maxa) maxa = s.[i].ave;
-        if(s.[i].avs>maxa) maxa = s.[i].avs;
-        if(s.[i].avh>maxa) maxa = s.[i].avh;
-    }
-    printf("Top student: %s with %d ",s[max].name,s[max].avg);
-    printf("subject Averages:\n");
-    printf("math: %.2f\n",s[i].avm);
-    printf("English: %.2f\n",s[i].ave);
-    printf("science: %.2f\n",s[i].avs);
-    printf("History: %.2f\n",s[i].avh);
+    for (int i = 0; i < 3; i++) {
+        printf("Enter name, ID, and 4 grades for student %d:\n", i + 1);
+        scanf("%20s %d %f %f %f %f",
+              st[i].name, &st[i].id, &st[i].m, &st[i].e, &st[i].s, &st[i].h);
 
-    printf("top subject: ")
+        
+        st[i].avg = (st[i].m + st[i].e + st[i].s + st[i].h) / 4;
+
+        
+        sumM += st[i].m;
+        sumE += st[i].e;
+        sumS += st[i].s;
+        sumH += st[i].h;
+
+        printf("\n"); 
+    }
+
+    
+    printf("Student Averages:\n");
+    int topIdx = 0;
+    for (int i = 0; i < 3; i++) {
+        printf("%s (ID: %d): %.2f\n", st[i].name, st[i].id, st[i].avg);
+        if (st[i].avg > st[topIdx].avg) topIdx = i;
+    }
+    printf("\n");
+    printf("Top Student: %s with %.2f\n\n", st[topIdx].name, st[topIdx].avg);
+
+   
+    subjAvg[0] = sumM / 3;
+    subjAvg[1] = sumE / 3;
+    subjAvg[2] = sumS / 3;
+    subjAvg[3] = sumH / 3;
+
+    printf("Subject Averages:\n");
+    for (int j = 0; j < 4; j++) {
+        printf("%s: %.2f\n", subjName[j], subjAvg[j]);
+    }
+    printf("\n");
+
+    
+    int topSubj = 0;
+    for (int j = 1; j < 4; j++) {
+        if (subjAvg[j] > subjAvg[topSubj]) topSubj = j;
+    }
+    printf("Top Subject: %s with average %.2f\n", subjName[topSubj], subjAvg[topSubj]);
+
     return 0;
 }
